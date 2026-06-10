4ca580d981becbae7edc87eb2431254b9524e061
ovrme <roe30611@gmail.com>
2026-06-10 20:12:48 +0700

    b

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,9 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    if ($this->app->environment('production')) {
-        URL::forceScheme('https');
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-}
 }
